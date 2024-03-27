0aa6815ac62d671ce19a264fb0381b000d154e65
nikolay <kolunya0@gmail.com>
2024-03-27 16:16:16 +0300

    laptop work

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 export default class MoleGame {
     constructor(fieldSize) {
-        this.fieldSize = fieldSize ** 2;
+        this.fieldSize = fieldSize ** 2
+        this.score = 0;
+        this.miss = 0;
     }
 
     // создание игрового поля исходя из указанного fieldSize
@@ -29,7 +31,28 @@ export default class MoleGame {
             holes[rand].classList.add('hole_has-mole'); // Была ошибка здесь, нужно добавить 'hole_' перед 'has-mole'
         }, 1000);
     }
+
+    logicHit() {
+        const counterHit = document.createElement('div');
+        counterHit.classList.add('counter');
+        counterHit.textContent = `Количество попаданий: ${this.score}`;
+        const gameCell = document.querySelector('.content');
+        const firstHole = gameCell.firstElementChild;
+        gameCell.insertBefore(counterHit, firstHole);
+        const holes = document.querySelectorAll('.hole');
+        holes.forEach((hole) => {
+            hole.addEventListener('onclick', () => {
+                if (hole.contains('hole_has-mole')) {
+                    this.score ++;
+                    console.log(this.score)
+                } else {
+                    this.miss ++;
+                }
+            })
+        })
+    }
 }
 
 const newGame = new MoleGame(4);
 newGame.creatingField();
+newGame.logicHit();
